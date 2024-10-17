f51ecf5faf8fdca9f637cfe6292f5c2ca3f7a419
ndkid <nadilday@yahoo.com>
2024-10-17 16:14:35 -0400

    - removing *.import files from checkin, as they're autogenerated by godot - moving landed squads from PlanetFaction to Region

--- Models/Planets/PlanetFaction.cs
+++ Models/Planets/PlanetFaction.cs
@@ -11,13 +11,11 @@ namespace OnlyWar.Models.Planets
         public int PDFMembers { get; set; }
         public float PlayerReputation { get; set; }
         public int PlanetaryControl { get; set; }
-        public List<Squad> LandedSquads { get; set; }
         public Character Leader { get; set; }
 
         public PlanetFaction(Faction faction)
         {
             Faction = faction;
-            LandedSquads = new List<Squad>();
             IsPublic = true;
             Population = 0;
             PDFMembers = 0;
--- Models/Planets/Region.cs
+++ Models/Planets/Region.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿using OnlyWar.Models.Squads;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OnlyWar.Models.Planets
 {
     public class Region
     {
         public readonly Planet Planet;
+        public readonly PlanetFaction OwningFaction;
+        // territory is diamond-shaped
+        // 1
+        // 2 3
+        // 4 5 6
+        // 7 8 9 10
+        // 11 12 13
+        // 14 15
+        // 16
         public readonly Tuple<ushort, ushort> Position;
+        public List<Squad> LandedSquads { get; set; }
     }
 }
