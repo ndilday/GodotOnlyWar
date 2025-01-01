7436384ac96779e58ca2aadfb36dd6a359472ea9
Nathan <nadilday@yahoo.com>
2025-01-01 18:07:23 -0500

    - adding a WarpLane class for later use - adding some art assets I plan to use - building out a rough concept for a soldier data panel

---
+++ Models/Planets/WarpLane.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OnlyWar.Models.Planets
+{
+    public class WarpLane
+    {
+        public readonly int Id;
+        public readonly int Quality;
+        public readonly Tuple<Planet, Planet> Path;
+
+        public WarpLane(int id, int quality, Tuple<Planet, Planet> path)
+        {
+            Id = id;
+            Quality = quality;
+            Path = path;
+        }
+    }
+}
