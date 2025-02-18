16589ae27a081e69c8a0f8fa4fd57bdbe5b808b6
Nathan <nadilday@yahoo.com>
2025-02-17 21:35:56 -0500

    - more noodling on special mission generation

--- Helpers/Missions/Recon/PerformReconMissionStep.cs
+++ Helpers/Missions/Recon/PerformReconMissionStep.cs
@@ -19,46 +19,16 @@ namespace OnlyWar.Helpers.Missions.Recon
             BaseSkill perception = GameDataSingleton.Instance.GameRulesData.BaseSkillMap.Values.First(s => s.Name == "Tactics");
             _missionTest = new SquadMissionTest(perception, 12.5f);
             StepIfSuccess = new ReconStealthMissionStep();
-            StepIfFailure = new ReconStealthMissionStep();
+            StepIfFailure = null;
         }
 
         public void ExecuteMissionStep(MissionContext context, float marginOfSuccess, IMissionStep returnStep)
         {
+            // move the generation of new missions to the turn controller, rather than the individual mission steps
             float margin = _missionTest.RunMissionTest(context.PlayerSquads);
-            if (margin > 0.0f)
-            {
-                float specMissionChance = margin;
-                // add current inelligence level
-                specMissionChance += context.Region.IntelligenceLevel;
-                // subtract one for each special mission already identified
-                specMissionChance -= context.Region.SpecialMissions.Count;
-                specMissionChance -= context.MissionsToAdd.Count;
-                specMissionChance += (float)RNG.NextRandomZValue();
-                // TODO: add some kind of recon data to the context
-                // do some sort of test to see whether a special mission opportunity is found
-                // if not, improve the inteligence level by the margin
-                if (specMissionChance <= 0)
-                {
-                    context.Region.IntelligenceLevel += margin / 5;
-                }
-                else if(specMissionChance >= 2)
-                {
-                    // assassination
-                }
-                else if(specMissionChance >= 1)
-                {
-                    // sabotage
-                }
-                else if (specMissionChance >= 0)
-                {
-                    // ambush, equipment/prisoner recovery
-                }
-                StepIfSuccess.ExecuteMissionStep(context, 0, returnStep);
-            }
-            else
-            {
-                StepIfFailure.ExecuteMissionStep(context, 0, returnStep);
-            }
+            context.Region.IntelligenceLevel += margin;
+            StepIfSuccess.ExecuteMissionStep(context, margin, this);
+
         }
     }
 }
--- Helpers/Sector/TurnController.cs
+++ Helpers/Sector/TurnController.cs
@@ -21,6 +21,43 @@ namespace OnlyWar.Helpers.Sector
                 MissionContext context = new MissionContext(order.TargetRegion, order.MissionType, new List<Squad> { order.OrderedSquad }, new List<Squad>());
                 MissionStepOrchestrator.GetStartingStep(context).ExecuteMissionStep(context, 0, null);
             }
+            foreach(Planet planet in sector.Planets.Values)
+            {
+                foreach(Region region in planet.Regions)
+                {
+                    if(region.IntelligenceLevel > 0)
+                    {
+                        // see if any intelligence gets spent in exchange for special mission opportunities
+                        float specMissionChance = region.IntelligenceLevel;
+                        // subtract one for each special mission already identified
+                        specMissionChance -= region.SpecialMissions.Count;
+                        specMissionChance += (float)RNG.NextRandomZValue();
+                        // TODO: add some kind of recon data to the context
+                        // do some sort of test to see whether a special mission opportunity is found
+                        // if not, improve the inteligence level by the margin
+                        if (specMissionChance >= 2)
+                        {
+                            // assassination
+                        }
+                        else if (specMissionChance >= 1)
+                        {
+                            // sabotage
+                            // plant minefield
+
+                        }
+                        else if (specMissionChance >= 0)
+                        {
+                            // ambush, equipment/prisoner recovery
+                            // sniper's nest
+                            // prisoner recovery
+                            // equipment recovery
+
+                        }
+                        // reduce intelligence level by 25%
+                        region.IntelligenceLevel *= 0.75f;
+                    }
+                }
+            }
         }
 
         private Dictionary<Region, List<Squad>> MapSquadsToTargetRegions(Planet planet)
