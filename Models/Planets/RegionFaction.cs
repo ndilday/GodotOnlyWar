712f5db3e12e546541b9158298c27fec7a62c1bd
Nathan <nadilday@yahoo.com>
2025-02-23 22:14:44 -0500

    - upped the effectiveness of sniper rifles - started adding more values to RegionFaction that local forces will be able to build up and special missions will be able to reduce

--- Models/Planets/RegionFaction.cs
+++ Models/Planets/RegionFaction.cs
@@ -12,6 +12,15 @@ namespace OnlyWar.Models.Planets
         public long Population { get; set; }
         public int PDFMembers { get; set; }
         public bool IsPublic { get; set; }
+        // Entrenchment provides bonsues against attacks
+        public float Entrenchment { get; set; }
+        // Detection provides bonuses to detecting enemy forces in the region
+        public float Detection { get; set; }
+        // AntiAir provides bonuses against air atacks and air assaults
+        public float AntiAir { get; set; }
+        // I'm not sure what organization will do yet, but it's what assassination should reduce
+        // perhaps it'll factor into size of enemy forces faced in battle
+        public float Organization { get; set; }
 
         public RegionFaction(PlanetFaction planetFaction, Region region)
         {
